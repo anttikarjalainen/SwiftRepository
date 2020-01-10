9abc751e04346566512145ece277a086afadbb33
Antti Karjalainen <1701542@APO-iMAC-29.local>
2020-01-10 09:30:06 +0200

    first commit

---
+++ SwiftTutorial/SwiftTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftTutorial
+//
+//  Created by Antti Karjalainen on 10/01/2020.
+//  Copyright © 2020 Antti Karjalainen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
